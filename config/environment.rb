dc85bb4f558142d3d24ecf75abfedaa7d5fdf2e9
Abishek Viswanathan <abishek.v@gmail.com>
2013-04-09 18:16:59 -0700

    First save

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+require 'yelp'
+require 'rubygems'
+require 'oauth'
+# Initialize the rails application
+Hungry1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hungry1::Application.config.secret_token = '6c9612f4272b77a8569b23a3631bfff512e53a99ce224be1d748a25f25f9ff54479b8fff38a9a66a625151e1299e922eea8a06dd77060addcdfcfaed46c6905a'
